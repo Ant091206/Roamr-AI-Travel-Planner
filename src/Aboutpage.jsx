03892ce569455750d1244b7b5555d56e80cb3674
Aryan <aaryanthakkar06@gmail.com>
2026-06-28 18:54:49 +0530

    Fix case sensitivity

--- src/Aboutpage.jsx
+++ src/Aboutpage.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { Wordmark } from './shared.jsx';
+import { Wordmark } from './Shared.jsx';
 import React from 'react';
 import aryanPhoto from './assets/Aryan photo professional.png';
 
